2318836ff34db184fa21bcb0ef3ffd46231916d7
1ceto <65932893+1ceto@users.noreply.github.com>
2025-07-03 17:06:20 +0800

    Create model.py

---
+++ model.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+
+class ResidualDenseBlock(nn.Module):
+    def __init__(self, nf=64, gc=32):
+        super().__init__()
+        self.conv1 = nn.Conv2d(nf, gc, 3, 1, 1)
+        self.conv2 = nn.Conv2d(nf + gc, gc, 3, 1, 1)
+        self.conv3 = nn.Conv2d(nf + 2 * gc, gc, 3, 1, 1)
+        self.conv4 = nn.Conv2d(nf + 3 * gc, gc, 3, 1, 1)
+        self.conv5 = nn.Conv2d(nf + 4 * gc, nf, 3, 1, 1)
+        self.lrelu = nn.LeakyReLU(0.2, inplace=True)
+
+    def forward(self, x):
+        x1 = self.lrelu(self.conv1(x))
+        x2 = self.lrelu(self.conv2(torch.cat([x, x1], 1)))
+        x3 = self.lrelu(self.conv3(torch.cat([x, x1, x2], 1)))
+        x4 = self.lrelu(self.conv4(torch.cat([x, x1, x2, x3], 1)))
+        x5 = self.conv5(torch.cat([x, x1, x2, x3, x4], 1))
+        return x + x5 * 0.2
+
+class RRDB(nn.Module):
+    def __init__(self, nf, gc):
+        super().__init__()
+        self.rdb1 = ResidualDenseBlock(nf, gc)
+        self.rdb2 = ResidualDenseBlock(nf, gc)
+        self.rdb3 = ResidualDenseBlock(nf, gc)
+
+    def forward(self, x):
+        out = self.rdb1(x)
+        out = self.rdb2(out)
+        out = self.rdb3(out)
+        return x + out * 0.2
+
+class GeoRealESRGANLite(nn.Module):
+    def __init__(self, in_nc=3, out_nc=3, nf=64, nb=4, gc=32):
+        super().__init__()
+        self.conv_first = nn.Conv2d(in_nc, nf, 3, 1, 1)
+        self.RRDB_trunk = nn.Sequential(*[RRDB(nf, gc) for _ in range(nb)])
+        self.trunk_conv = nn.Conv2d(nf, nf, 3, 1, 1)
+
+        self.upsample = nn.Sequential(
+            nn.Conv2d(nf, nf * 4, 3, 1, 1),
+            nn.PixelShuffle(2),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+
+        self.conv_last = nn.Conv2d(nf, out_nc, 3, 1, 1)
+
+    def forward(self, x):
+        fea = self.conv_first(x)
+        trunk = self.trunk_conv(self.RRDB_trunk(fea))
+        fea = fea + trunk
+        fea = self.upsample(fea)
+        out = self.conv_last(fea)
+        return out
